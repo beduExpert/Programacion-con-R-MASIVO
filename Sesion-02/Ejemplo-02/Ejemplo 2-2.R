67c7fe8d91a9a1a7b2d248855e23bd3ea4ce1eb6
jennerfr <50311949+jennerfr@users.noreply.github.com>
2021-01-14 14:51:51 -0600

    Update Ejemplo 2-2.R

--- Sesion-02/Ejemplo-02/Ejemplo 2-2.R
+++ Sesion-02/Ejemplo-02/Ejemplo 2-2.R
@@ -54,4 +54,20 @@ tail(mtcars)
 
 View(iris)
 
+# Función que calcula la moda
+
+# En R también podemos crear nuestras propias funciones, por ejemplo
+
+moda <- function(vector){
+f.abs <- table(vector) # frecuencias absolutas
+max.f.abs <- max(f.abs) # obtenemos la máxima frecuencia absoluta
+pos.max <- which(f.abs == max.f.abs) # posición(es) de la(s) máxima(s) frecuencia(s) absoluta(s)
+print("La(s) moda(s) es(son): ")
+print(names(f.abs[pos.max]))
+paste("Con una frecuencia de: ", unique(as.vector(f.abs[pos.max])))
+}
+
+x <- sample(1:100, 100, replace = T) # Tomamos una muestra aleatoria de tamaño 100 con reemplazo de los primeros 100 números naturales
+table(x) # obtenemos las frecuencias absolutas de los valores de la muestra
+moda(x) # obtenemos la moda de los valores de la muestra
 
