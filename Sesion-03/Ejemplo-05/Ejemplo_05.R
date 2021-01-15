0c7b79f369ab795c8bda06485277523cba4bfedd
jennerfr <50311949+jennerfr@users.noreply.github.com>
2021-01-15 00:52:05 -0600

    Update Ejemplo_05.R

--- Sesion-03/Ejemplo-05/Ejemplo_05.R
+++ Sesion-03/Ejemplo-05/Ejemplo_05.R
@@ -2,14 +2,14 @@
 
 # install.packages("scales")
 
-library(scales)
-# Ahora vamos a leer nuestro archivo con los resultados de la variable mex con los infectados y muertos acumulados para cada fecha
+library(scales) # Para mejorar la lectura de las etiquetas  en el eje de las x de tipo fecha
+# Ahora vamos a leer nuestro archivo C19Mexico.csv con los infectados y muertos acumulados para cada fecha
 
 mex <- read.csv("C19Mexico.csv")
 
 head(mex); tail(mex)
 
-mex <- mutate(mex, Date = as.Date(Date, "%Y-%m-%d"))
+mex <- mutate(mex, Fecha = as.Date(Fecha, "%Y-%m-%d"))
 str(mex)
 
 ####################################################################
@@ -18,7 +18,7 @@ str(mex)
 
 # Acumulado de Casos Confirmados
 
-p <- ggplot(mex, aes(x=Date, y=Infectados)) + 
+p <- ggplot(mex, aes(x=Fecha, y=Infectados)) + 
   geom_line( color="blue") + 
   geom_point() +
   labs(x = "Fecha", 
@@ -41,12 +41,12 @@ p <- p  + scale_x_date(labels = date_format("%d-%m-%Y")) # paquete scales
 
 p <- p +
   theme(plot.margin=margin(10,10,20,10), plot.caption=element_text(hjust=1.05, size=10)) +
-  annotate("text", x = mex$Date[round(dim(mex)[1]*0.4)], y = max(mex$Infectados), colour = "blue", size = 5, label = paste("Última actualización: ", mex$Infectados[dim(mex)[1]]))
+  annotate("text", x = mex$Fecha[round(dim(mex)[1]*0.4)], y = max(mex$Infectados), colour = "blue", size = 5, label = paste("Última actualización: ", mex$Infectados[dim(mex)[1]]))
 p
 
 # Casos Confirmados por Día
 
-p <- ggplot(mex, aes(x=Date, y=NI)) + 
+p <- ggplot(mex, aes(x=Fecha, y=NI)) + 
   geom_line(stat = "identity") + 
   labs(x = "Fecha", y = "Incidencia (Número de casos nuevos)",
        title = paste("Casos de Incidencia de COVID-19 en México:", 
@@ -63,7 +63,7 @@ p
 
 p <- p +
   theme(plot.margin=margin(10,10,20,10), plot.caption=element_text(hjust=1.05, size=10)) +
-  annotate("text", x = mex$Date[round(dim(mex)[1]*0.4)], y = max(mex$NI), colour = "blue", size = 5, 
+  annotate("text", x = mex$Fecha[round(dim(mex)[1]*0.4)], y = max(mex$NI), colour = "blue", size = 5, 
            label = paste("Última actualización: ", mex$NI[length(mex$NI)]))
 p
 
@@ -71,7 +71,7 @@ p
 
 mexm <- subset(mex, Muertos > 0) # Tomamos el subconjunto desde que comenzaron las muertes
 
-p <- ggplot(mexm, aes(x=Date, y=Muertos)) + geom_line( color="red") + 
+p <- ggplot(mexm, aes(x=Fecha, y=Muertos)) + geom_line( color="red") + 
   geom_point() +
   labs(x = "Fecha", 
        y = "Muertes acumuladas",
@@ -87,12 +87,12 @@ p
 
 p <- p +
   theme(plot.margin=margin(10,10,20,10), plot.caption=element_text(hjust=1.05, size=10)) +
-  annotate("text", x = mexm$Date[round(dim(mexm)[1]*0.4)], 
+  annotate("text", x = mexm$Fecha[round(dim(mexm)[1]*0.4)], 
            y = max(mexm$Muertos), colour = "red", size = 5, label = paste("Última actualización: ", mexm$Muertos[dim(mexm)[1]]))
 p
 
 # Muertes por Día
-p <- ggplot(mexm, aes(x=Date, y=NM)) + 
+p <- ggplot(mexm, aes(x=Fecha, y=NM)) + 
   geom_line(stat = "identity") + 
   labs(x = "Fecha", y = "Número de nuevos decesos",
        title = paste("Nuevos decesos por COVID-19 en México:", 
@@ -107,12 +107,12 @@ p <- p  + scale_x_date(labels = date_format("%d-%m-%Y"))
 
 p <- p +
   theme(plot.margin=margin(10,10,20,10), plot.caption=element_text(hjust=1.05, size=10)) +
-  annotate("text", x = mexm$Date[round(dim(mexm)[1]*0.2)], 
+  annotate("text", x = mexm$Fecha[round(dim(mexm)[1]*0.2)], 
            y = max(mexm$NM), colour = "red", size = 5, label = paste("Última actualización: ", mexm$NM[dim(mexm)[1]]))
 p
 
 # Acumulado de Casos Confirmados y Muertes
-p <- ggplot(mex, aes(x=Date, y=Infectados)) + geom_line(color="blue") + 
+p <- ggplot(mex, aes(x=Fecha, y=Infectados)) + geom_line(color="blue") + 
   labs(x = "Fecha", 
        y = "Acumulado de casos",
        title = paste("COVID-19 en México:", format(Sys.time(), tz="America/Mexico_City",usetz=TRUE))) +
@@ -126,15 +126,15 @@ p <- p  + scale_x_date(labels = date_format("%d-%m-%Y"))
 
 p <- p +
   theme(plot.margin=margin(10,10,20,10), plot.caption=element_text(hjust=1.05, size=10)) +
-  annotate("text", x = mex$Date[round(dim(mex)[1]*0.4)], 
+  annotate("text", x = mex$Fecha[round(dim(mex)[1]*0.4)], 
            y = max(mex$Infectados), colour = "blue", size = 5, label = paste("Última actualización para Infectados:", mex$Infectados[dim(mex)[1]])) +
-  annotate("text", x = mex$Date[round(dim(mex)[1]*0.4)], 
+  annotate("text", x = mex$Fecha[round(dim(mex)[1]*0.4)], 
            y = max(mex$Infectados)-100000, colour = "red", size = 5, label = paste("Última actualización para Muertes:", mex$Muertos[dim(mex)[1]])) 
 p
 
 # Tasa de Letalidad: La tasa de letalidad observada para un día determinado, la calculamos dividiendo las muertes acumuladas reportadas hasta ese día, entre el acumulado de casos confirmados para el mismo día. Multiplicamos el resultado por 100 para reportarlo en forma de porcentaje. Lo que obtenemos es el porcentaje de muertes del total de casos confirmados.
 
-p <- ggplot(mexm, aes(x=Date, y=Letalidad)) + geom_line(color="red") + 
+p <- ggplot(mexm, aes(x=Fecha, y=Letalidad)) + geom_line(color="red") + 
   labs(x = "Fecha", 
        y = "Tasa de letalidad",
        title = paste("COVID-19 en México:", format(Sys.time(), tz="America/Mexico_City",usetz=TRUE))) +
@@ -149,7 +149,7 @@ p <- p  + scale_x_date(labels = date_format("%d-%m-%Y"))
 
 p <- p +
   theme(plot.margin=margin(10,10,20,10), plot.caption=element_text(hjust=1.05, size=10)) +
-  annotate("text", x = mexm$Date[round(length(mexm$Date)*0.2)], 
+  annotate("text", x = mexm$Fecha[round(length(mexm$Fecha)*0.2)], 
            y = max(mexm$Letalidad)-1, colour = "red", size = 4, label = paste("Última actualización: ", mexm$Letalidad[dim(mexm)[1]], "%", sep = "")) 
 p
 
@@ -159,7 +159,7 @@ p
 
 mex <- filter(mex, FCM < Inf) # Tomamos solo valores reales de factores de crecimiento
 
-p <- ggplot(mex, aes(x=Date, y=FCI)) + geom_line(color="blue") + 
+p <- ggplot(mex, aes(x=Fecha, y=FCI)) + geom_line(color="blue") + 
   labs(x = "Fecha", 
        y = "Factor de crecimiento",
        title = paste("COVID-19 en México:", format(Sys.time(), tz="America/Mexico_City",usetz=TRUE))) +
@@ -172,6 +172,6 @@ p <- p  + scale_x_date(labels = date_format("%d-%m-%Y"))
 ###
 
 p <- p +
-  annotate("text", x = mex$Date[round(length(mex$Date)*0.4)], y = max(mex$FCM), colour = "blue", size = 5, label = paste("Última actualización para infectados: ", round(mex$FCI[dim(mex)[1]], 4))) +
-  annotate("text", x = mex$Date[round(length(mex$Date)*0.4)], y = max(mex$FCM)-0.2, colour = "red", size = 5, label = paste("Última actualización para muertes: ", round(mex$FCM[dim(mex)[1]], 4))) 
+  annotate("text", x = mex$Fecha[round(length(mex$Fecha)*0.4)], y = max(mex$FCM), colour = "blue", size = 5, label = paste("Última actualización para infectados: ", round(mex$FCI[dim(mex)[1]], 4))) +
+  annotate("text", x = mex$Fecha[round(length(mex$Fecha)*0.4)], y = max(mex$FCM)-0.2, colour = "red", size = 5, label = paste("Última actualización para muertes: ", round(mex$FCM[dim(mex)[1]], 4))) 
 p
