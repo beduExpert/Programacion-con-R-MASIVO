0b734399d85078bb95f561bd4efea9eb8f4aa08a
jennerfr <50311949+jennerfr@users.noreply.github.com>
2021-01-20 00:46:04 -0600

    Update Ejemplo_04.R

--- Sesion-03/Ejemplo-04/Ejemplo_04.R
+++ Sesion-03/Ejemplo-04/Ejemplo_04.R
@@ -1,16 +1,11 @@
 # Comenzamos leyendo un fichero, el cual contiene información sobre dos grupos de control G1 y G2, a los cuales se les realizó a cada uno una medición en 3 momentos diferentes C1, C2 y C3
 
-data2 <- read.csv("../Sesion_03/boxp.csv")
+data <- read.csv("../Sesion_03/boxp.csv")
 
 # Revisamos el encabezado del fichero y el nombre de sus variables o columnas
 
-head(data2)
-names(data2)
-
-# Vamos a realizar un cambio en la variable Mediciones para practicar
-
-data <- mutate(data2, Mediciones = Mediciones*1.23)
 head(data)
+names(data)
 
 # Observamos algunos datos estadísticos sobre las variables
 
