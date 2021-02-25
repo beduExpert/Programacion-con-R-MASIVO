d8533fa85882302714e453370d342738212c9c76
jennerfr <50311949+jennerfr@users.noreply.github.com>
2021-02-25 13:10:26 -0600

    Update Ejemplo_03.R

--- Sesion-05/Ejemplo-03/Ejemplo_03.R
+++ Sesion-05/Ejemplo-03/Ejemplo_03.R
@@ -39,6 +39,7 @@ ggplot(Default[-train, ],
   theme_light() + ggtitle("Conjunto de prueba")
 
 # 4. Ahora utilicemos la función tune junto con la función svm para seleccionar el mejor modelo de un conjunto de modelos, los modelos considerados serán aquellos obtenidos al variar los valores de los parámetros cost y gamma (usaremos un kernel radial).
+# Nota: se recomienda mucho correr el código que combina la función `tune` y la función `svm` antes de la sesión del work, esto porque se requiere mucho tiempo para que se obtengan los resultados.
 
 # Ahora utilizamos la función `tune` junto con la función `svm` para 
 # seleccionar el mejor modelo de un conjunto de modelos, los modelos 
